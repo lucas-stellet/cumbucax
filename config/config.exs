55d5c34f0bb7a1f26423b98370262b81258d2ece
Lucas Stellet <lustepe@gmail.com>
2022-04-14 21:54:28 -0300

    chore: create money config

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Money default current configuration
+config :money,
+  default_currency: :BRL
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
