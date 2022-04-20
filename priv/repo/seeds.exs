75f2eedd673c1c630b8c4d9ed4b4f569ba2e2407
Lucas Stellet <lustepe@gmail.com>
2022-04-20 10:06:49 -0300

    chore: add User and Account seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,30 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Cumbucax.Repo.insert!(%Cumbucax.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Seeds do
+  alias Cumbucax.Repo
+
+  alias Cumbucax.Accounts.Account
+  alias Cumbucax.Users.User
+
+  def run(_repo) do
+    user_1 =
+      Repo.insert!(%User{
+        cpf: "001.002.003-04",
+        first_name: "John",
+        last_name: "Doe",
+        password_hash:
+          "$argon2id$v=19$m=65536,t=8,p=2$5lVFTBqK5NhI2z5xr1j/bA$LPKvma+WVvjCf2tdOzbNXuUTivvHJauYCET26W0cfn0"
+      })
+
+    Repo.insert!(%Account{
+      id: Ecto.UUID.generate(),
+      branch: "0001",
+      number: "135790",
+      digit: "2",
+      balance: Money.new(100),
+      user_id: user_1.id
+    })
+  end
+end
+
+Cumbucax.Release.load_app()
+
+Ecto.Migrator.with_repo(Cumbucax.Repo, &Seeds.run/1)
