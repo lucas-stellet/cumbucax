6d2e9818c5e2479b9a4e5d94f97b970dcfda3d0e
Lucas Stellet <lustepe@gmail.com>
2022-04-22 16:57:03 -0300

    chore: add user and transaction migrations

--- priv/repo/migrations/20220414203000_create_users.exs
+++ priv/repo/migrations/20220414203000_create_users.exs
@@ -11,7 +11,5 @@ defmodule Cumbucax.Repo.Migrations.CreateUsers do
 
       timestamps()
     end
-
-    create unique_index(:users, [:cpf])
   end
 end
--- priv/repo/migrations/20220415004436_create_transaction.exs
+++ priv/repo/migrations/20220415004436_create_transaction.exs
@@ -5,16 +5,14 @@ defmodule Cumbucax.Repo.Migrations.CreateTransaction do
     create table(:transaction, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :amount, :integer
-      add :overturned, :boolean, default: false, null: false
-      add :overturned_at, :naive_datetime
       add :status, :string
-      add :beneficiary_id, references(:accounts, on_delete: :nothing, type: :binary_id)
-      add :requester_id, references(:accounts, on_delete: :nothing, type: :binary_id)
+      add :beneficiary_account_id, references(:accounts, on_delete: :nothing, type: :binary_id)
+      add :requester_account_id, references(:accounts, on_delete: :nothing, type: :binary_id)
 
       timestamps()
     end
 
-    create index(:transaction, [:beneficiary_id])
-    create index(:transaction, [:requester_id])
+    create index(:transaction, [:beneficiary_account_id])
+    create index(:transaction, [:requester_account_id])
   end
 end
