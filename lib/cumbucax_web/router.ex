19db25e13db43494f3f6f7df8f97064cd4f18e00
Lucas Stellet <lustepe@gmail.com>
2022-04-22 17:14:08 -0300

    feat: organize routes with scopes and pipelines

--- lib/cumbucax_web/router.ex
+++ lib/cumbucax_web/router.ex
@@ -5,7 +5,26 @@ defmodule CumbucaxWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug CumbucaxWeb.Auth.Pipeline
+  end
+
+  pipeline :bodyguard do
+    plug CumbucaxWeb.BodyGuard
+  end
+
   scope "/api", CumbucaxWeb do
     pipe_through :api
+
+    post "/register", CumbucaxController, :register
+    post "/login", CumbucaxController, :login
+  end
+
+  scope "/api", CumbucaxWeb do
+    pipe_through [:api, :auth, :bodyguard]
+
+    post "/transactions", CumbucaxController, :transfer
+    get "/transactions", CumbucaxController, :list
+    patch "/transactions/refund", CumbucaxController, :refund
   end
 end
